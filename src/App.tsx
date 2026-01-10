eb9febdfbc3e530984c14c36521aa4397b681c3a
Mohanad Mohamed <71340065+MohanadAhmed123@users.noreply.github.com>
2026-01-11 00:23:27 +0200

    Create Vite Project and Setup Initial Files
    Created a new Vite Project with React and Typescript
    and installed the dependencies for react router and supabase
    and tailwind css. Also added the initial folder/file structure.

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="min-h-screen flex items-center justify-center bg-black text-white">
+      <h1 className="text-4xl font-bold">Muna Gift App</h1>
+    </div>
+  )
+}
+
+export default App
